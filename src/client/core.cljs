e98ee34b7ac74af869de183137053c4f5b58f134
Gaz Liddon Retina <gaz@gazaxian.com>
2017-04-05 16:21:55 +0100

    WIP

--- src/client/core.cljs
+++ src/client/core.cljs
@@ -2,8 +2,8 @@
   (:require
     [client.game :as game]
 
-    [sablono.core :as html :refer-macros [html]]
 
+    [sablono.core :as html :refer-macros [html]]
     [com.stuartsierra.component :as c]
 
     [servalan.protocols.clientconnection :as client]
@@ -13,7 +13,7 @@
 
     [servalan.fsm :as fsm]
     [client.protocols :as p] 
-    [client.html :as client-html]
+    [client.html :refer [mk-html-component mk-html-events-component]]
     [client.utils :refer [ch->coll cos cos01] :as u]
 
     [client.keys :as k]
@@ -33,7 +33,9 @@
 
   (:require-macros 
     [servalan.macros :as m :refer [dochan]]
-    [cljs.core.async.macros :refer [go go-loop]]))
+    [cljs.core.async.macros :refer [go go-loop]]
+    
+    ))
 
 
 (def config { :conn-config {:url  "ws://localhost:6502"
@@ -48,11 +50,15 @@
 
     :config config
 
-    :system (client-html/mk-html-component (:html-id config))
+    :system (mk-html-component (:html-id config))
 
-    :events (client-html/mk-html-events-component )
+    :events (mk-html-events-component )
 
-    :client-connection (mk-client-component (:conn-config config))
+    :client-connection (c/using
+                        ( mk-client-component (:conn-config config) )
+                        [:com-chan]
+                        
+                        )
 
     :game (c/using (game/mk-game)
                           [:client-connection
@@ -77,8 +83,8 @@
     (->>
       (mk-game config)
       (c/start-system) 
-      (reset! sys-atom )
-      )
+      (reset! sys-atom ))
+    (-> @sys-atom :client-connection client/connect!)
     )
   )
 
@@ -106,68 +112,9 @@
 ;; =============================================================================
 ;; Objs
 
-(defn is-active? [{:keys [start-time duration]} t]
-  (and (>= t start-time) (< t (+ start-time duration))))
-
-(defn perc-through [{:keys [start-time duration]} t]
-  (/ (- t start-time) duration ))
-
-(defmulti draw-obj (fn [obj t ctx]
-                     (:type obj)) )
-
-(defmethod draw-obj :frutz [{:keys [pos col]} t ctx]
-  (let [[x y] pos
-        new-pos [(+ x (* 100 (cos01 (* 3 t )))) y] ]
-    (p/square! ctx new-pos [30 30] col) ))
-
-(defmethod draw-obj :player [{:keys [pos col]} t ctx]
-  (p/square! ctx pos [30 30] col))
-
-(defn draw-objs
-  "convert these objs into a draw list"
-  [objs t ctx]
-  (dorun
-    (->
-      (fn [o]
-        (when (is-active? o t)
-          (let [my-t (- (:start-time o) t)]
-            (draw-obj o my-t ctx) )))
-      (map objs))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; player
-
-(def player (atom {:pos [10 10]}))
-
-(def k->dir {
-   :z [-1 0]
-   :x [ 1 0]
-   :k [0 -1]
-   :m [0 1] })
-
-(defn addv2 [[x y] [x1 y1]] [(+ x x1) (+ y y1)])
-
-(defn keys->player [player key-coll]
-  (->
-    (fn [res {:keys [event keypress]}]
-      (if (= event :keydown)
-        (let [v (get k->dir keypress [0 0])
-              p (addv2 (:pos res) v) ]
-          (assoc res :pos p))
-        res))
-    (reduce player key-coll)))
-
-(comment defn update! [io timer ]
-  (let [renderer (renderer io)
-        now (/ ( now timer ) 1000    )
-        col [(int (* 255 (cos01 (* 10 now )) )) 0 0]
-        new-t (mod now 5)
-        new-player (keys->player @player (keyboard io)) ]
-    (do
-      (clear-buffer! renderer  col)
-      (draw-objs @objs @t renderer  )
-      (square! renderer (:pos new-player) [10 10] [255 255 255] )
-      (reset! player new-player))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -241,30 +188,47 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn mk-button [this txt transaction action]
-  (dom/button
-    #js {:class "pure-button pure-button-primary" 
-         :onClick (fn [e]
-                    (action)
-                    (om/transact! this transaction))}
-    txt))
+  [:button
+   {:class  "pure-button pure-button-primary"
+    :on-click (fn []
+                (action)
+                (om/transact! this transaction)) }
+   txt]
+
+  ; [:button
+  ;  {:class  "pure-button pure-button-primary"
+
+  ;   :on-click (fn []
+  ;               (action)
+  ;               (om/transact! this transaction)) }
+  ;  txt]
+  )
+
+(defn game-running-buttons [this]
+  )
 
 (defui App
 
   static om/IQuery
 
   (query [this]
-
-         [:game-running])
+         '[:game-running])
   Object
 
   (render [this]
-          (let [{:keys [game-running] :as props} (om/props this)]
+          (let [{:keys [game-running] :as props} (om/props this) ]
+            (html
+              [:div
+               [:div [:h1 "Cheg"]]
 
-            (dom/div nil
-                     (if game-running
-                       (mk-button this "stop game" `[(game/stop ~props)] stop)
-                       (mk-button this "start game" `[(game/start ~props)] start))
-                     ))))
+               (if game-running
+
+                 [:div
+                  [:div (mk-button this "Quit" `[(game/stop)] stop)] 
+                  [:div (mk-button this "Connect" `[] #())] ]
+
+                 [:div
+                  (mk-button this "Start" `[(game/start)] start) ]) ] ))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
--- src/client/game.cljs
+++ src/client/game.cljs
@@ -17,15 +17,84 @@
     [servalan.macros :as m :refer [dochan]]
     [cljs.core.async.macros :refer [go go-loop]]))
 
+
+(def player (atom {:pos [10 10]}))
+
+(def k->dir {
+   :z [-1 0]
+   :x [ 1 0]
+   :k [0 -1]
+   :m [0 1] })
+
+(defn addv2 [[x y] [x1 y1]] [(+ x x1) (+ y y1)])
+
+(defn keys->player [player key-coll]
+  (->
+    (fn [res {:keys [event keypress]}]
+      (if (= event :keydown)
+        (let [v (get k->dir keypress [0 0])
+              p (addv2 (:pos res) v) ]
+          (assoc res :pos p))
+        res))
+    (reduce player key-coll)))
+
+
+(comment defn update! [io timer ]
+  (let [renderer (renderer io)
+        now (/ ( now timer ) 1000    )
+        col [(int (* 255 (cos01 (* 10 now )) )) 0 0]
+        new-t (mod now 5)
+        new-player (keys->player @player (keyboard io)) ]
+
+    (do
+      (clear-buffer! renderer  col)
+      (draw-objs @objs @t renderer  )
+      (square! renderer (:pos new-player) [10 10] [255 255 255] )
+      (reset! player new-player))))
+
+(defn is-active? [{:keys [start-time duration]} t]
+  (and (>= t start-time) (< t (+ start-time duration))))
+
+(defn perc-through [{:keys [start-time duration]} t]
+  (/ (- t start-time) duration ))
+
+(defmulti draw-obj (fn [obj t ctx]
+                     (:type obj)) )
+
+(defmethod draw-obj :frutz [{:keys [pos col]} t ctx]
+  (let [[x y] pos
+        new-pos [(+ x (* 100 (cos01 (* 3 t )))) y] ]
+    (p/square! ctx new-pos [30 30] col) ))
+
+(defmethod draw-obj :player [{:keys [pos col]} t ctx]
+  (p/square! ctx pos [30 30] col))
+
+(defn draw-objs
+  "convert these objs into a draw list"
+  [ctx objs t]
+  (dorun
+    (->
+      (fn [o]
+        (when (is-active? o t)
+          (let [my-t (- (:start-time o) t)]
+            (draw-obj o my-t ctx) )))
+      (map objs))))
+
+(def objs (atom []))
+
 (defn print! [renderer t]
   (let [red (* 255 (cos01 (*  t 10)))
         col [red 0 255]
         fcol (apply u/to-color col) ]
 
     (do
-      (p/clear-all! renderer col)
-      )))
+      (doto renderer
 
+        (p/clear-all! col)
+        (draw-objs @objs t)
+        (p/square! (:pos @player) [10 10] [255 255 255] )
+        
+        ))))
 
 (defprotocol IGame
   (on-network [_ msg])
